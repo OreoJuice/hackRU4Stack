1ab7025e31a72d203f4fe7560e6f578b136710f4
NATHANIEL ENDICK <nathaniel.endick@gmail.com>
2023-10-08 00:45:14 -0400

    Font Spinner Setup, Default Values (except Spinner)

--- app/src/main/java/com/example/hackru/SettingsActivity.java
+++ app/src/main/java/com/example/hackru/SettingsActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 
@@ -13,6 +18,7 @@ import static com.google.android.material.internal.ContextUtils.getActivity;
 
 public class SettingsActivity extends AppCompatActivity{
 
+    private EditText nameEditText;
     private Switch weatherWidgetSwitch;
     private Switch celsiusSwitch;
     private Switch motivationalQuotesWidget;
@@ -29,19 +35,57 @@ public class SettingsActivity extends AppCompatActivity{
         sharedPrefEditor = sharedPref.edit();
 
         instantiateViewObjects();
+//        Log.d("123SettingsActivity", sharedPref.getString(getString(R.string.user_name),""));
     }
 
     private void instantiateViewObjects() {
+        //Name
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                sharedPrefEditor.putString(getString(R.string.user_name),nameEditText.getText().toString());
+                sharedPrefEditor.apply();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+        nameEditText.setText(sharedPref.getString(getString(R.string.user_name),""));
+
         //Weather Widget
         weatherWidgetSwitch = (Switch)findViewById(R.id.weatherWidgetSwitch);
-        celsiusSwitch = (Switch)findViewById(R.id.celsiusSwitch);
         weatherWidgetSwitch.setChecked(sharedPref.getBoolean(getString(R.string.weather_widget_active), true));
+        weatherWidgetSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPrefEditor.putBoolean(getString(R.string.weather_widget_active), isChecked);
+                sharedPrefEditor.apply();
+            }
+        });
+
+        celsiusSwitch = (Switch)findViewById(R.id.celsiusSwitch);
         celsiusSwitch.setChecked(sharedPref.getBoolean(getString(R.string.celsius), true));
+        celsiusSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPrefEditor.putBoolean(getString(R.string.celsius), isChecked);
+                sharedPrefEditor.apply();
+            }
+        });
 
         //Motivational Quotes Widget
         motivationalQuotesWidget = (Switch)findViewById(R.id.motivationalQuotesWidget);
-        fontFamilySpinner = (Spinner)findViewById(R.id.fontFamilySpinner);
         motivationalQuotesWidget.setChecked(sharedPref.getBoolean(getString(R.string.motivational_quote_widget_active), true));
+        motivationalQuotesWidget.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPrefEditor.putBoolean(getString(R.string.motivational_quote_widget_active), isChecked);
+                sharedPrefEditor.apply();
+            }
+        });
+
+        String[] values = getResources().getStringArray(R.array.font_family_choices);
+        fontFamilySpinner = (Spinner)findViewById(R.id.fontFamilySpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontFamilySpinner.setAdapter(adapter);
         //TODO set up spinner values and store the current spinner value
         //        fontFamilySpinner.setSelection(0);
     }
