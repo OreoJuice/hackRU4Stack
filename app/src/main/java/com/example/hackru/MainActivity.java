e1c1f499f297b912704375ddf1f639d48e71fab6
NATHANIEL ENDICK <nathaniel.endick@gmail.com>
2023-10-08 01:43:43 -0400

    Weather Fragment Working (hopefully)

--- app/src/main/java/com/example/hackru/MainActivity.java
+++ app/src/main/java/com/example/hackru/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.hackru;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -29,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
         SharedPreferences pf = getPreferences(MODE_PRIVATE);
         boolean weather = pf.getBoolean(getString(R.string.weather_widget_active),true);
@@ -37,26 +28,8 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_quotes, QuotesFragment.class, null)
-                    .add(R.id.fragment_container_weather, WeatherFragment.class, null)
+                    .add(R.id.fragmentContainerView2, WeatherFragment.class, null)
                     .commit();
         }
-    }
-
-    public String serializeUser(User myUser){
-        SharedPreferences sp = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor pe = sp.edit();
-        Gson gson = new Gson();
-        String jUser = gson.toJson(myUser);
-        pe.putString("MyUser", jUser);
-        pe.apply();
-        return jUser;
-    }
-
-    public User deserializeSessionFromJson() {
-        Gson gson = new Gson();
-        SharedPreferences sp = getPreferences(MODE_PRIVATE);
-        String jUser = sp.getString("MyUser", null);
-        return gson.fromJson(jUser, User.class);
     }
 }
