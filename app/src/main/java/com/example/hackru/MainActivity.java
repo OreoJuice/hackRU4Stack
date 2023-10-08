c5851e4cb0ff3b03b0d74ce63b41e430a7b11319
LaurenceB2022 <107589657+LaurenceB2022@users.noreply.github.com>
2023-10-07 23:35:45 -0400

    Updated MainActivity.java

--- app/src/main/java/com/example/hackru/MainActivity.java
+++ app/src/main/java/com/example/hackru/MainActivity.java
@@ -35,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         layout.setId(R.id.linear_layout);
         FragmentManager fragMan = getFragmentManager();
         FragmentTransaction fragTransaction = fragMan.beginTransaction();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_weather, WeatherFragment.newInstance("Weather1", "Weather2"))
+                    .replace(R.id.fragment_container_quotes, QuotesFragment.newInstance("Quote1", "Quote2"))
+                    .commitNow();
+        }
 
 
 
