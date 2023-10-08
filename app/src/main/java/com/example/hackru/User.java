83546b5f2a3da336884d95cb75f91d690b81190e
NATHANIEL ENDICK <nathaniel.endick@gmail.com>
2023-10-07 23:18:19 -0400

    Merged changes

--- app/src/main/java/com/example/hackru/User.java
+++ app/src/main/java/com/example/hackru/User.java
@@ -5,23 +5,9 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String firstName;
     private String lastName;
-    private String town;
 
-    public User(String firstName, String lastName, String town){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.town = town;
+    public User(){
+
     }
 
-    public void updateInfo(String type, String value){
-        if(type.equals(firstName)){
-            firstName = value;
-        }
-        if(type.equals(lastName)){
-            lastName = value;
-        }
-        if(type.equals(town)){
-            town = value;
-        }
-    }
 }
